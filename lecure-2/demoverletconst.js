49a718c84a4d9ea377491782354a3c7238d4584d
bagpipe6666 <bumpoloyou@gmail.com>
2022-12-21 11:01:59 +0700

    update

---
+++ lecure-2/demoverletconst.js
@@ -0,0 +1,17 @@
+var hello = "Hello";
+
+console.log(hello);
+hello = "Hello World";
+console.log(hello);
+
+if (true){
+    let World = "Hello World";
+    console.log(World);
+
+}
+//console.log (World);
+
+const fixval = "Fix Value";
+console.log(fixval);
+
+//fix = "tes assingne value to const"
---
+++ lecure-2/null.js
@@ -0,0 +1,9 @@
+function getVowels(str) {
+    const m = str.match(/[aeiou]/gi);
+    if (m === null){
+        return 0;
+    }
+    return m.length;
+}
+
+console.log(getVowels("seeing"));
