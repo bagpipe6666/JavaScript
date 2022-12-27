41bf7ab51d6499765dd400a0211c8071b2d7d237
bagpipe6666 <94041532+bagpipe6666@users.noreply.github.com>
2022-12-28 00:36:00 +0700

    update

---
+++ lecure-2/strsplit.js
@@ -0,0 +1,7 @@
+const str = 'The quick brown fox jumps over the lazy dog';
+
+const world = str.split('');
+console.log(world[3]);
+
+const chars = str.split('');
+console.log(chars[8]);
---
+++ lecure-3/json-obj/jsonDemo.js
@@ -0,0 +1,24 @@
+/* START */
+
+// HOW can you represent real-world objects in code ? 
+// They hace associated attributes = object PROPERITES
+// They have associated actions = object METHODS
+// They have associated context = "this"
+
+//First;
+// Let's define a simple object ( with no properties or methods)
+const blank = {};
+console.log("Blank type : " , typeof blank);
+console.log("Blank value " ,blank);
+
+/*
+// Next :
+// Let's define an object with PROPERTIES (attributes)
+// Obj are just a collection of name-value pairs
+// separated by commas
+const book = {
+    title : "1984",
+    author : "George Orwell", 
+    isAvailable : false
+};
+*/
