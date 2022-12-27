6d6b62ea700a9aeb64577b13732a1ad890ecaa0b
bagpipe6666 <94041532+bagpipe6666@users.noreply.github.com>
2022-12-27 23:42:39 +0700

    Create maparray.js

---
+++ lecure-3/maparray.js
@@ -0,0 +1,7 @@
+const finalParticipants = ["taylor","Donald","Don","Natasha","Bobby"];
+
+const announcements = finalParticipants.map((mamber)=>{
+    return member + " joined the constest";
+
+});
+console.log(announcements);
