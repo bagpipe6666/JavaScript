849aa60d7a88ea90a1a8ca6d1abfa1bc46cec88a
bagpipe6666 <bumpoloyou@gmail.com>
2023-01-25 11:16:53 +0700

    add

---
+++ teat2.js
@@ -0,0 +1,77 @@
+// numberSplit(4);
+// numberSplit(10);
+// numberSplit(11);
+// numberSplit(-9);
+
+// switch (new Data()){
+//     case 0:
+//         data = 4/2;
+//     case 1:
+//         data = 10 /2;
+//     case 2:
+//         data = 11/2;
+//     case 3:
+//         data = 0;
+                
+// }
+
+
+
+//รับมาจับหารจําตัวมาก  หรือ เปลี่ยนเป็นสตริงเเล้วจับหา
+// const highestDigit = "379";
+// const highestDigit2 = "2";
+// const highestDigit3 = "377401";
+
+// function highestNum(n){
+//     if (n==0){
+//         return 0;
+//     }
+//     else{
+//         return Math.max(n%10, highestNum(n/10));
+//     }
+// }
+
+// console.log(highestNum(379));
+// console.log(highestNum(2));
+// console.log(Math.floor(highestNum(377401)));
+
+//array move char 
+// let capToFront = "hApPy";
+// let capToFront1 = "moveMENT";
+// let capToFront2 = "shOrtCAke";
+
+//     if ("A" in data) data.remove(data.indexOf("MENT")); 
+//         data.unshift("role")
+// data;
+
+// totalVolme = ([4,2,4],[3,3,3],[1,1,2],[2,1,1]);
+// totalVolme = ([2,2,2],[2,1,1]);
+// totalVolme = ([1,1,1]);
+  
+// sumToSmalllestNums = ({19,5,42,2,77)
+
+
+
+// let digitalcloock = (5025);
+// let digitalcloock2 = (5025);
+// let digitalcloock3 = (5025);
+
+// function
+
+
+
+//get bugject
+
+// let removeDups = [1,0,1,0];
+// let removeDups2 = (['the','big','big']);
+// let removeDups3 = (["john","taylor","john"]);
+
+// let uniqueChars2 = [...new Set(removeDups2)];
+// let uniqueChars3 = [...new Set(removeDups3)];
+
+// console.log(uniqueChars2);
+// console.log(uniqueChars3);
+
+
+
+
